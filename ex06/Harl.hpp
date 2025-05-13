6033bdcef4a99051ea42f1f8a34a90988e4f32d5
sudaniel <sudaniel@student.42heilbronn.de>
2025-05-13 09:28:36 +0200

    Implement Ex06

---
+++ ex06/Harl.hpp
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Harl.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sudaniel <sudaniel@student.42heilbronn.de  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/12 14:18:21 by sudaniel          #+#    #+#             */
+/*   Updated: 2025/05/13 09:08:42 by sudaniel         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef	HARL_HPP
+#define	HARL_HPP
+
+#include <iostream>
+#include <string>
+
+struct Harl
+{
+	using	 PMF = void (Harl::*)(void);
+	enum	levels {DEBUG, INFO, WARNING, ERROR, DEFAULT};
+	void	complain(std::string level);
+	int		mapLevels(std::string& level);
+ private:
+	 void	debug(void);
+	 void	info(void);
+	 void	warning(void);
+	 void	error(void);
+};
+
+#endif
