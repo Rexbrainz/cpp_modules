0cca766fb223058c877269c93e51d595fa80c6f1
sudaniel <sudaniel@student.42heilbronn.de>
2025-05-13 13:45:50 +0200

    Fix empty string1 cases

--- ex04/main.cpp
+++ ex04/main.cpp
@@ -6,7 +6,7 @@
 /*   By: sudaniel <sudaniel@student.42heilbronn.de  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/12 12:19:54 by sudaniel          #+#    #+#             */
-/*   Updated: 2025/05/12 13:57:52 by sudaniel         ###   ########.fr       */
+/*   Updated: 2025/05/13 13:38:37 by sudaniel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,20 +31,37 @@ void	process_file(std::istream& in, std::ostream& out,
 	}
 }
 
+bool	isArgumentsValid(int argc, char **argv)
+{
+	if (argc != 4)
+	{
+		std::cout << "\033[31mError: Wrong input\033[0m";
+		std::cout << std::endl;
+		return (false);
+	}
+	if (!*argv[2])
+	{
+		std::cout << "\033[31mError: String to remove cannot be empty\033[0m\n";
+		return (false);
+	}
+	return (true);
+}
+
 int	main(int argc, char **argv)
 {
 	std::ifstream	in;
 	std::ofstream	out;
 	std::string		file;
 
-	if (argc != 4)
-	{
-		std::cout << "\033[31mError: Wrong input\033[0m";
-		std::cout << std::endl;
+	if (!isArgumentsValid(argc, argv))
 		return (0);
-	}
 	file = argv[1];
 	in.open(file);
+	if (!in)
+	{
+		std::cout << "\033[31mError: File does not exist.\033[0m\n";
+		return (0);
+	}
 	out.open(file + ".replace", std::ofstream::app);
 	process_file(in, out, argv[2], argv[3]);
 	in.close();
