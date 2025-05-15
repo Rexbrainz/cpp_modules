ffade752e9df25b6cca92009a16606df9ab10605
sudaniel <sudaniel@student.42heilbronn.de>
2025-05-15 11:01:15 +0200

    Implement Ex00

--- ex00/Fixed.cpp
+++ ex00/Fixed.cpp
@@ -6,7 +6,7 @@
 /*   By: sudaniel <sudaniel@student.42heilbronn.de  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/14 17:10:39 by sudaniel          #+#    #+#             */
-/*   Updated: 2025/05/14 17:48:03 by sudaniel         ###   ########.fr       */
+/*   Updated: 2025/05/15 10:57:38 by sudaniel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,4 +14,36 @@
 
 const int Fixed::fBits = 8;
 
-Fixed::Fixed() : fpnValue(0) {}
+Fixed::Fixed(void) : fpnValue(0)
+{
+	std::cout << "Default constructor called" << std::endl;
+}
+
+Fixed::Fixed(const Fixed& other) : fpnValue(other.getRawBits())
+{
+	std::cout << "Copy constructor called" << std::endl;
+}
+
+Fixed&	Fixed::operator=(const Fixed& rhs)
+{
+	std::cout << "Copy assignment operator called" << std::endl;
+	fpnValue = rhs.getRawBits();
+	return (*this);
+}
+
+Fixed::~Fixed(void)
+{
+	std::cout << "Destructor called" << std::endl;
+}
+
+int	Fixed::getRawBits(void) const
+{
+	std::cout << "getRawBits member function called" << std::endl;
+	return (fpnValue);
+}
+
+void	Fixed::setRawBits(int const raw)
+{
+	std::cout << "setRawBits member function called" << std::endl;
+	fpnValue = raw;
+}
--- ex00/main.cpp
+++ ex00/main.cpp
@@ -1,32 +1,28 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   Fixed.hpp                                          :+:      :+:    :+:   */
+/*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: sudaniel <sudaniel@student.42heilbronn.de  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/05/14 16:45:00 by sudaniel          #+#    #+#             */
-/*   Updated: 2025/05/15 10:48:04 by sudaniel         ###   ########.fr       */
+/*   Created: 2025/05/15 08:59:58 by sudaniel          #+#    #+#             */
+/*   Updated: 2025/05/15 10:59:33 by sudaniel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef	FIXED_HPP
-#define	FIXED_HPP
+#include "Fixed.hpp"
 
-#include <iostream>
-#include <string>
-
-struct	Fixed
+int	main(void)
 {
-	Fixed(void);
-	Fixed(const Fixed&);
-	Fixed&	operator=(const Fixed&);
-	~Fixed();
-	int		getRawBits(void) const;
-	void	setRawBits(int const raw);
-  private:
-	int					fpnValue;
-	static const int	fBits;
-};
+	Fixed	a;
+	Fixed	b(a);
+	Fixed	c;
 
-#endif
+	c = b;
+
+	std::cout << a.getRawBits() << std::endl;
+	std::cout << b.getRawBits() << std::endl;
+	std::cout << c.getRawBits() << std::endl;
+
+	return (0);
+}
